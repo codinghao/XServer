72f1bcc292f00744fa26e13ff9629e105e955265
codinghao@gmail.com <codinghao@gmail.com>
2013-10-18 19:39:45 +0800

    add peeraddr.h

---
+++ GameFrame/src/Common/PeerAddr.h
@@ -0,0 +1,32 @@
+#ifndef _TCP_ADDR_H_
+#define _TCP_ADDR_H_
+
+class PeerAddr
+{
+public:
+    PeerAddr(const char* _ip, ushort _port, ushort _family = AF_INET)
+    {
+        m_Addr.sin_family = _family;
+        m_Addr.sin_addr.s_addr = ::inet_addr(_ip);
+        m_Addr.sin_port = ::htons(_port);
+    }
+
+    const sockaddr* operator()()
+    {
+        return (sockaddr*)&m_Addr;
+    }
+
+    const char* Ip() 
+    {
+        return inet_ntoa(m_Addr);
+    }
+
+    const ushort Port()
+    {
+        return ::ntohs(m_Addr.sin_port);
+    }
+private:
+    struct sockaddr_in m_Addr;
+};
+
+#endif
